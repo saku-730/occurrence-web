0d7d2ba31e57b72d7e0bd0424983fa6ffac57c1e
saku-730 <119673139+saku-730@users.noreply.github.com>
2026-08-31 14:34:09 +0900

    add paper occurrence registration page

---
+++ frontend/src/app/paper-import/register/page.tsx
@@ -0,0 +1,23 @@
+import { PaperOccurrenceRegisterClient } from "./paper-occurrence-register-client";
+
+type SearchParams = Promise<{
+  paperId?: string;
+  scientificName?: string;
+  locality?: string;
+}>;
+
+export default async function PaperOccurrenceRegisterPage({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const params = await searchParams;
+
+  return (
+    <PaperOccurrenceRegisterClient
+      paperId={params.paperId ?? ""}
+      scientificName={params.scientificName ?? ""}
+      locality={params.locality ?? ""}
+    />
+  );
+}
